50c3e48d2eb0159aca937712a569acaad21387c0
Robert Programming <Programming@Roberts-Macbook.home>
2023-12-01 08:12:25 +0100

    minor change

--- 2023_1.py
+++ 2023_1.py
@@ -4,6 +4,6 @@ from aoc import *
 
 print(load(1,2023).findgroups('\d').applyto(lambda x: (x[0],x[-1])).combine().toint().sum())
 
-pats = r'(zero|one|two|three|four|five|six|seven|eight|nine)'
-mapper = {x: str(i) for i, x in enumerate(pats[1:-1].split('|'))}
-print(load(1,2023).findgroups(r'(zero|one|two|three|four|five|six|seven|eight|nine|\d)',overlap=True).replace(mapper).applyto(lambda x: (x[0],x[-1])).combine().toint().sum())
+pats = 'zero|one|two|three|four|five|six|seven|eight|nine'
+mapper = {x: str(i) for i, x in enumerate(pats.split('|'))}
+print(load(1,2023).findgroups(f'({pats}|\d)',overlap=True).replace(mapper).applyto(lambda x: (x[0],x[-1])).combine().toint().sum())
