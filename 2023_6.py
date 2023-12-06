2cc84c8cb87ec4166771c56fdbde46581994b47d
Robert Programming <Programming@Roberts-Macbook.home>
2023-12-06 09:56:59 +0100

    shorter version

--- 2023_6.py
+++ 2023_6.py
@@ -1,18 +1,13 @@
 from aoc import *
 from math import *
+import numpy as np
 
 data = load(6,2023).findgroups('\d+').toint()
-T = data[0]
-D = data[1]
 
-nways = []
-for t,d in zip(T,D):
-    D = d+0.001
-    t1 = ceil((t-sqrt(t**2-4*D))/2)
-    t2 = floor((t+sqrt(t**2-4*D))/2)
-    nways.append(t2-t1+1)
-    
-print(f'Part 1 = {product(nways)} ways')
+T = np.array(data[0].items)
+D = np.array(data[1].items)
+print('Part 1 short = ',product(np.floor((T+np.sqrt(T**2-4*(D+0.001)))/2) - np.ceil((T-np.sqrt(T**2-4*(D+0.001)))/2)+1))
+
 
 data = load(6,2023,False).findgroups('\d+').combine().toint()
 T = data[0]
