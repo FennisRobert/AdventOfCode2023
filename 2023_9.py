858b5fd6b69c8fba4f50c2c4b9c3b8cba9d7a840
Robert Programming <Programming@Roberts-Macbook.home>
2023-12-09 09:29:10 +0100

    make all the plots

--- 2023_9.py
+++ 2023_9.py
@@ -1,6 +1,7 @@
 from aoc import *
 import numpy as np
 from scipy.linalg import solve
+import matplotlib.pyplot as plt
 
 data = (load(9,2023,False).split(' ').toint())
 dtype = np.float64
@@ -15,6 +16,35 @@ def extrapoly(ys: np.ndarray, order: int, querypoint):
     coeff = solve(M, B)
     return sum(coeff*(querypoint**np.arange(order,-1,-1).astype(np.float64)))
 
+
+def plotsol(ys: np.ndarray, order: int, querypoint):
+    xs = np.arange(len(ys)).astype(dtype)
+    N = order+1
+    M = np.zeros((N,N)).astype(dtype)
+    for i in range(N):
+        M[:,i] = xs[:N]**(order-i)
+    B = ys[:N]
+    coeff = solve(M, B)
+    
+    xs = xs[:5]
+    ys = ys[:5]
+    f = lambda x: x#np.log10(x)
+    plt.scatter(xs, f(ys))
+    DX = xs[-1]
+    xs2 = np.linspace(-1, xs[-1]+1.2,10000)
+
+    ys2 = sum([(xs2**(order-i))*coeff[i] for i in range(N)])
+    x3 = np.array([-1, xs[-1]+1])
+    y3 = sum([(x3**(order-i))*coeff[i] for i in range(N)])
+    
+    plt.plot(xs2,f(ys2))
+    plt.scatter(x3,f(y3))
+    plt.grid(True)
+    plt.show()
+    
+
+
+    
 def idiff(inpt: np.ndarray, times: int):
     outpt = inpt
     for i in range(times):
@@ -34,7 +64,10 @@ sumv1 = 0
 sumv2 = 0
 sumv1linalg = 0
 sumv2linalg = 0
-for _list in data:
+
+N=data.len-2
+
+for i, _list in enumerate(data):
     diffdata = np.array([x for x in _list])
     n = 0
     while not all([x==0 for x in diffdata]):
@@ -42,11 +75,15 @@ for _list in data:
         n += 1
     
     arry = np.array([x for x in _list])
+    
     sumv1 += extrapolate(arry,n)
     sumv2 += extrapolate(arry[::-1],n)
     
     sumv1linalg += extrapoly(arry,n,len(arry))
     sumv2linalg += extrapoly(arry,n,-1)
+    plotsol(arry,n,len(arry))
+
+    
 
     
 print(f'Part1: {sumv1}, Part2: {sumv2}')
