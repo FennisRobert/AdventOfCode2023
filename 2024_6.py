f5dffe7a032ef254a60d94aa3d288f9f7567d1f8
Robert Programming <Programming@MacBookPro.home>
2024-12-06 13:32:16 +0100

    cleanup of day 6 code

--- 2024_6.py
+++ 2024_6.py
@@ -3,30 +3,16 @@ from tqdm import tqdm
 ######## PART 1 #######
 data = load(6,2024,test=False).split().tomatrix().pad(1,'F').replace({'.':''})
 
-viewer = data.copy()
-
 guard = [(i,j) for i in range(data.width) for j in range(data.height) if data[j][i]=='^'][0]
 
 x,y = guard
 dx,dy = 0,-1
 counter = 1
-viewer[y][x] = 'X'
+
 havebeen = set([(x,y)])
 
 path_states = list()
 
-pss = {
-    (0,1): '│',
-    (1,0): '─',
-    (0,-1): '│',
-    (-1,0): '─',
-}
-rotpss = {
-    (0,1): '┘',
-    (1,0): '┐',
-    (0,-1): '┌',
-    (-1,0): '└',
-}
 rotations = []
 while True:
     
@@ -40,17 +26,14 @@ while True:
     else:
         path_states.append((x,y,dx,dy))
         x,y = nx, ny
-        viewer[ny][nx] = pss[(dx,dy)]
         if not (x,y) in havebeen:
             counter += 1
             havebeen.add((x,y))
-    
-    #print(viewer.tostring())
-print(viewer.tostring())
 
 print(f'Solution to part 1: {counter}')
 
 ######## PART 2 #######
+
 import numpy as np
 mapper = {
     '': 0,
@@ -59,11 +42,11 @@ mapper = {
     '^': 0
 }
 data = np.array([[mapper[x] for x in row] for row in data.dt])
-print(data)
-viewer = data.copy()
+
 coords = [(x,y) for x,y,_,_ in path_states]
 
 loop_options = set()
+
 for x0,y0,dx0,dy0 in tqdm(path_states):
     
     testfield = 1*data
@@ -82,22 +65,16 @@ for x0,y0,dx0,dy0 in tqdm(path_states):
         
         if (x,y,dx,dy) in havebeen:
             loop_options.add((x0+dx0,y0+dy0))
-            #print(viewer.tostring())
             break
         
         havebeen.add((x,y,dx,dy))
         if testfield[ny,nx]==2:
             break
         elif testfield[ny,nx]==1:
-            #viewer[y][x] = rotpss[(dx,dy)]
             dx, dy = -dy, dx
-            #rotations.append((x,y))
             continue
         else:
             x,y = nx, ny
-            #viewer[ny][nx] = pss[(dx,dy)]
-            
-
 
 print(f'Solution to part 2: {len(loop_options)}')
 
