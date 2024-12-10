d0181384ac98e5aed488686f87e8279df46f6dba
Robert Programming <Programming@MacBookPro.home>
2024-12-10 09:21:28 +0100

    Cleaned up version of day 10

--- 2024_10.py
+++ 2024_10.py
@@ -2,32 +2,25 @@ from aoc import *
 from collections import defaultdict
 
 ######## PART 1 #######
-data = load(10,2024,test=False).split().toint().tomatrix()
+data = load(10,2024,test=False).split().toint().tomatrix().pad(1,-1)
 
 W = data.width
 H = data.height
 
 trailheads = defaultdict(list)
 
-def find_paths(current_height: int, current_pos: tuple, current_path: list, paths: dict):
-    x,y = current_pos
+def find_paths(current_height: int, current_path: list, paths: dict):
+    x,y = current_path[-1]
     if current_height == 9:
         paths[(x,y)].append(current_path)
         return paths
     for xn,yn in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
-        if not data.inside(xn,yn):
-            continue
         if data[yn][xn] == current_height + 1:
-            paths = find_paths(current_height+1, (xn,yn), current_path + [(xn,yn),], paths)
+            paths = find_paths(current_height+1, current_path + [(xn,yn),], paths)
     return paths
 
-for x,y in [(x,y) for x in range(W) for y in range(H)]:
-    if data[y][x] != 0:
-        continue
-    paths = defaultdict(list)
-    paths = find_paths(0, (x,y), [(x,y)], paths)
-    trailheads[(x,y)] = paths
-    
+for x,y in data.find(0):
+    trailheads[(x,y)] = find_paths(0, [(x,y)], defaultdict(list))
 
 counter_part_1 = 0
 counter_part_2 = 0
